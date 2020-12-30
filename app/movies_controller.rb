0e4551bd0c8d9e1f7c09e730794802b856bb0645
Jose Melero Martinez <jmelero08@gmail.com>
2020-12-30 17:06:29 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,15 +42,15 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last 
 end
 
 def can_get_size_of_the_database
-  __
+  Moive.count 
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  
 end
 
 def can_find_by_multiple_attributes
